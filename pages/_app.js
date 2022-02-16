ec682476472c11402f2e2b0ebbced718417a5f44
Melissa Tan <62129448+melissatzt@users.noreply.github.com>
2022-02-16 13:18:20 +1000

    feat: fb google login function (#6)
    * feat: facebook google login
    
    * feat: google fb login
    
    * Delete .env
    
    * Delete package-lock.json
    
    Co-authored-by: Alan <68600416+Qiming-Liu@users.noreply.github.com>

--- pages/_app.js
+++ pages/_app.js
@@ -1,28 +1,33 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { SessionProvider } from 'next-auth/react';
 import Head from 'next/head';
 import store from '../store/store';
 import LoginDialog from '../components/login/LoginDialog';
 import Login from '../components/login';
+import LoginFBGoogle from '../components/thirdpartylogin';
 import '../styles/main.scss';
 
-const MyApp = ({ Component, pageProps }) => {
+const MyApp = ({ Component, pageProps: { session, ...pageProps } }) => {
   return (
     <Provider store={store}>
-      <Head>
-        <title>ThinkMoreForum</title>
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/icon?family=Material+Icons"
-        />
-      </Head>
-      <Component {...pageProps} />
-      <LoginDialog>
-        <Login />
-      </LoginDialog>
+      <SessionProvider session={session}>
+        <Head>
+          <title>ThinkMoreForum</title>
+          <meta name="viewport" content="initial-scale=1, width=device-width" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/icon?family=Material+Icons"
+          />
+        </Head>
+        <Component {...pageProps} />
+        <LoginDialog>
+          <Login />
+        </LoginDialog>
+        <LoginFBGoogle />
+      </SessionProvider>
     </Provider>
   );
 };
---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,20 @@
+import NextAuth from 'next-auth';
+import FacebookProvider from 'next-auth/providers/facebook';
+import GoogleProvider from 'next-auth/providers/google';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+export default NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_ID,
+      clientSecret: process.env.FACEBOOK_SECRET,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+  ],
+});
