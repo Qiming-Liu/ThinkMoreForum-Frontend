40f08c86184c1a2b163b4146db0361a8f4d29f5e
Melissa Tan <62129448+melissatzt@users.noreply.github.com>
2022-02-18 00:03:42 +1000

    feat: encrypt env file (#9)
    * feat: encrypt .env file
    
    * Delete package-lock.json

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,17 +4,21 @@ import GoogleProvider from 'next-auth/providers/google';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import { PrismaClient } from '@prisma/client';
 
+const secureEnv = require('secure-env');
+
+global.env = secureEnv({ secret: 'mySecretPassword' });
+
 const prisma = new PrismaClient();
 export default NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [
     FacebookProvider({
-      clientId: process.env.FACEBOOK_ID,
-      clientSecret: process.env.FACEBOOK_SECRET,
+      clientId: global.env.FACEBOOK_ID,
+      clientSecret: global.env.FACEBOOK_SECRET,
     }),
     GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
+      clientId: global.env.GOOGLE_ID,
+      clientSecret: global.env.GOOGLE_SECRET,
     }),
   ],
 });
