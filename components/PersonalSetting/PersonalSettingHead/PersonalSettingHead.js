58277b0fb1be927471a81fd16b03d0c2b141dd61
Shelton <89503179+dmwcs@users.noreply.github.com>
2022-02-19 22:12:55 +1030

    setting page structure (#15)
    Co-authored-by: “Shelton <“damwcs@gmail.com”>

---
+++ components/PersonalSetting/PersonalSettingHead/PersonalSettingHead.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+
+
+const PersonalSettingHead = () => {
+  
+  return (
+    <div>
+      <div >Account</div>
+    </div>
+  )
+}
+
+export default PersonalSettingHead;
