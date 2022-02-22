7212aea79f71bcc8609601eb609f63eee1460c96
Qiming-Liu <prossliu@gmail.com>
2022-02-22 16:16:27 +1030

    fix

---
+++ pages/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Grid } from '@mui/material';
+import Category from '../components/Categroy';
+
+const Index = () => {
+  return (
+    <Grid container spacing={4}>
+      <Category
+        type="A"
+        color="primary.main"
+        title="Category One"
+        description="description description description"
+        postCount="10"
+      />
+      <Category
+        type="B"
+        color="neutral.900"
+        title="Category Two"
+        description="description description description"
+        postCount="10"
+      />
+      <Category
+        type="A"
+        color="primary.main"
+        title="Category Three"
+        description="description description description"
+        postCount="10"
+      />
+      <Category
+        type="B"
+        color="neutral.900"
+        title="Category Four"
+        description="description description description"
+        postCount="10"
+      />
+    </Grid>
+  );
+};
+
+export default Index;
