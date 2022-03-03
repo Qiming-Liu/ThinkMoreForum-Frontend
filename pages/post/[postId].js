e248379041288f3d112782c17ce1d87e9fb8b268
zzyang1994 <yangzhanzhao1994@gmail.com>
2022-03-03 20:34:13 +1100

    feat: users can follow(favorite) a post now

--- pages/post/[postId].js
+++ pages/post/[postId].js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import { useRouter } from 'next/router';
 import { Button, Container, Divider } from '@mui/material';
 import NextLink from 'next/link';
@@ -6,6 +7,9 @@ import ArrowLeftIcon from '../../icons/arrow-left';
 import {
   getPostByPostId,
   getCommentByPost,
+  checkIsFavoringPost,
+  submitUnfavoritePost,
+  submitFavoritePost,
 } from '../../services/usersServices';
 import PostContent from '../../components/Post/PostContent';
 import ProfileComment from '../../components/Profile/ProfileComment';
@@ -15,6 +19,8 @@ const Post = () => {
   const { categoryTitle, postId } = router.query;
   const [post, setPost] = useState(null);
   const [comments, setComments] = useState([]);
+  const [postFaved, setPostFaved] = useState(false);
+  const { isLogin } = useSelector((state) => state.sign);
   const rootComments = comments.filter(
     (comment) => comment.parentComment === null,
   );
@@ -28,15 +34,35 @@ const Post = () => {
         (a, b) =>
           new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime(),
       );
+
+  const handleFavPost = async () => {
+    if (postFaved) {
+      const { data: response } = await submitUnfavoritePost(postId);
+      console.log(response);
+    } else {
+      const { data: response } = await submitFavoritePost(postId);
+      console.log(response);
+    }
+    setPostFaved(!postFaved);
+  };
+
   useEffect(() => {
-    const getPostContent = async () => {
-      const { data: responsePost } = await getPostByPostId(postId);
-      setPost(responsePost);
-      const { data: responseComments } = await getCommentByPost(postId);
-      setComments(responseComments);
-    };
-    getPostContent();
-  }, [postId]);
+    if (typeof postId !== 'undefined') {
+      const getPostContent = async () => {
+        const { data: responsePost } = await getPostByPostId(postId);
+        setPost(responsePost);
+        const { data: responseComments } = await getCommentByPost(postId);
+        setComments(responseComments);
+        if (isLogin) {
+          const { data: responseIsFavoringPost } = await checkIsFavoringPost(
+            postId,
+          );
+          setPostFaved(responseIsFavoringPost);
+        }
+      };
+      getPostContent();
+    }
+  }, [postId, postFaved, isLogin]);
   if (!post) return null;
   return (
     <Container maxWidth="md">
@@ -46,7 +72,11 @@ const Post = () => {
         </Button>
       </NextLink>
       <Divider sx={{ my: 3 }} />
-      <PostContent post={post} />
+      <PostContent
+        post={post}
+        isFavored={postFaved}
+        toggleFav={handleFavPost}
+      />
       {rootComments &&
         rootComments.map((rootComment) => {
           return (
