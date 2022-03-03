930d2c007372bf123700e5f03d72e1740e027665
Qiming-Liu <prossliu@gmail.com>
2022-03-03 21:18:55 +1030

    fix: build failed

--- components/Post/PostContent.js
+++ components/Post/PostContent.js
@@ -1,5 +1,4 @@
-/* eslint-disable no-unused-vars */
-import React, { useState } from 'react';
+import React from 'react';
 import Head from 'next/head';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import { useSelector } from 'react-redux';
