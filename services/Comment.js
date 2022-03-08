34065a4376d10d1fce2147a7b5aa1e1908ef0bc5
Alan <68600416+Qiming-Liu@users.noreply.github.com>
2022-03-08 18:50:33 +1030

    feat: save user detail in redux and show it on account popover (#75)

--- services/Comment.js
+++ services/Comment.js
@@ -1,7 +1,4 @@
 import http from '../utils/axios';
-<<<<<<< HEAD
 
 export const getPostCommentsByPostId = (postId) =>
   http(`/v1/comment/${postId}`, { method: 'GET' });
-=======
->>>>>>> 2a00e1d (fix: url)
