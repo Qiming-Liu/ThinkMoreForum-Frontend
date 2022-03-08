f50bffe512c4199bc740c1d657b543936130369e
jiangjiangzhouzhou <67827176+jiangjiangzhouzhou@users.noreply.github.com>
2022-03-08 19:15:29 +1100

    Tmf 161 image password post bug (#77)
    * bug fixed
    
    * feat: image, post, password bugs fixed

--- services/Users.js
+++ services/Users.js
@@ -3,7 +3,10 @@ import http from '../utils/axios';
 export const hasOpenid = () => http(`/v1/users/openid`, { method: 'GET' });
 
 export const passwordReset = (password) =>
-  http(`/v1/users/password-reset/${password}`, { method: 'PUT' });
+  http(`/v1/users/password-reset`, {
+    method: 'PUT',
+    data: { new_password: password },
+  });
 
 export const getMyUser = () => http(`/v1/users/my-details`, { method: 'GET' });
 
