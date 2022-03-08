4be5533df7358dd0192d61178c9732392de5b32c
Lesi Wei <61730243+wglssm@users.noreply.github.com>
2022-03-08 21:29:04 +1030

    feat: add comment and nested comment,disable comment (#76)
    * feat: add comment and nested comment, disable comment post when is not login
    
    * fix: remove JWT decode
    
    Co-authored-by: Alan <68600416+Qiming-Liu@users.noreply.github.com>

--- components/AntComment/index.js
+++ components/AntComment/index.js
@@ -1,22 +1,50 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import React, { useState } from 'react';
 import { Comment, Tooltip, Avatar } from 'antd';
 import myTime from '../../utils/myTime';
+import CommentForm from '../Post/CommentForm';
 
-const AntComment = ({ children, comment }) => {
-  const { commentUsers, context, createTimestamp } = comment;
+const AntComment = ({ comment, replies, sendChildComment, login }) => {
+  const { commentUsers, createTimestamp } = comment;
+  const [showReplying, setShowReplying] = useState(false);
+  const parrentId = comment.parentComment;
   return (
     <Comment
-      actions={[<span key="comment-nested-reply-to">Reply</span>]}
+      key={comment.id}
+      actions={[
+        // eslint-disable-next-line jsx-a11y/click-events-have-key-events
+        // eslint-disable-next-line jsx-a11y/no-static-element-interactions
+        parrentId === null ? (
+          // eslint-disable-next-line jsx-a11y/no-static-element-interactions
+          <span
+            key="comment-nested-reply-to"
+            onClick={() => setShowReplying(!showReplying)}
+          >
+            Reply
+          </span>
+        ) : (
+          <span> </span>
+        ),
+      ]}
       author={commentUsers.username}
       avatar={<Avatar src={commentUsers.profileImgUrl} />}
-      content={context}
+      content={comment.context}
       datetime={
         <Tooltip>
           <span>{myTime(createTimestamp)}</span>
         </Tooltip>
       }
     >
-      {children}
+      {showReplying ? (
+        <CommentForm
+          handleSubmit={(context) => sendChildComment(context, comment.id)}
+          login={login}
+        />
+      ) : null}
+      {replies !== null &&
+        replies.map((reply) => (
+          <AntComment comment={reply} key={reply.id} replies={[]} />
+        ))}
     </Comment>
   );
 };
