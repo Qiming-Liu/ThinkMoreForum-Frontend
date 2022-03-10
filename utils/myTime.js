6b5ee2c02159bf952226a313327e5051b3ef8466
jiangjiangzhouzhou <jiangjianglovezhou@gmail.com>
2022-03-10 23:03:25 +1100

    feat: fix my time bug

--- utils/myTime.js
+++ utils/myTime.js
@@ -3,12 +3,12 @@ import moment from 'moment';
 const MyTime = (timestamp) => {
   const thatTime = moment(timestamp);
   if (thatTime.isSame(moment(), 'day')) {
-    return thatTime.fromNow();
+    return thatTime.fromNow().toString();
   }
   if (thatTime.isAfter(moment().subtract(1, 'day'), 'day')) {
-    return thatTime.calendar();
+    return thatTime.calendar().toString();
   }
-  return moment(timestamp);
+  return moment(timestamp).format('MMMM Do YYYY, h:mm a').toString();
 };
 
 export default MyTime;
