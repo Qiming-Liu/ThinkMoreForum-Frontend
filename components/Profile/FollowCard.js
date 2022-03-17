aeed0be34496d0332cd40f5619c48badc58bfcfc
zzyang1994 <yangzhanzhao1994@gmail.com>
2022-03-17 21:39:02 +1100

    fix: code format

--- components/Profile/FollowCard.js
+++ components/Profile/FollowCard.js
@@ -21,15 +21,55 @@ const FollowCard = (props) => {
       <Divider />
       <Box sx={{ p: 3 }}>
         <Grid container spacing={3}>
-          {follow.map((fo) => {
-            console.log(fo.followedUsers.id);
-            return (
-              <Grid item key={fo.id} md={6} xs={12}>
-                <Paper sx={{ height: '100%' }} variant="outlined">
+          {follow.map((fo) => (
+            <Grid item key={fo.id} md={6} xs={12}>
+              <Paper sx={{ height: '100%' }} variant="outlined">
+                <Box
+                  sx={{
+                    display: 'flex',
+                    p: 2,
+                  }}
+                >
+                  {title === 'Following' && (
+                    <NextLink
+                      href={{
+                        pathname: `/profile/${fo.followedUsers.username}`,
+                        query: { userId: fo.followedUsers.id },
+                      }}
+                      passHref
+                    >
+                      <Avatar
+                        component="a"
+                        src={fo.followedUsers.headImgUrl}
+                        sx={{
+                          height: 56,
+                          width: 56,
+                        }}
+                      />
+                    </NextLink>
+                  )}
+                  {title === 'Follower' && (
+                    <NextLink
+                      href={{
+                        pathname: `/profile/${fo.users.username}`,
+                        query: { userId: fo.users.id },
+                      }}
+                      passHref
+                    >
+                      <Avatar
+                        component="a"
+                        src={fo.users.headImgUrl}
+                        sx={{
+                          height: 56,
+                          width: 56,
+                        }}
+                      />
+                    </NextLink>
+                  )}
                   <Box
                     sx={{
-                      display: 'flex',
-                      p: 2,
+                      flexGrow: 1,
+                      mx: 2,
                     }}
                   >
                     {title === 'Following' && (
@@ -40,14 +80,9 @@ const FollowCard = (props) => {
                         }}
                         passHref
                       >
-                        <Avatar
-                          component="a"
-                          src={fo.followedUsers.headImgUrl}
-                          sx={{
-                            height: 56,
-                            width: 56,
-                          }}
-                        />
+                        {/* <Link color="textPrimary" variant="subtitle2"> */}
+                        {fo.followedUsers.username}
+                        {/* </Link> */}
                       </NextLink>
                     )}
                     {title === 'Follower' && (
@@ -58,64 +93,26 @@ const FollowCard = (props) => {
                         }}
                         passHref
                       >
-                        <Avatar
-                          component="a"
-                          src={fo.users.headImgUrl}
-                          sx={{
-                            height: 56,
-                            width: 56,
-                          }}
-                        />
+                        {/* <Link color="textPrimary" variant="subtitle2"> */}
+                        {fo.users.username}
+                        {/* </Link> */}
                       </NextLink>
                     )}
-                    <Box
-                      sx={{
-                        flexGrow: 1,
-                        mx: 2,
-                      }}
-                    >
-                      {title === 'Following' && (
-                        <NextLink
-                          href={{
-                            pathname: `/profile/${fo.followedUsers.username}`,
-                            query: { userId: fo.followedUsers.id },
-                          }}
-                          passHref
-                        >
-                          {/* <Link color="textPrimary" variant="subtitle2"> */}
-                          {fo.followedUsers.username}
-                          {/* </Link> */}
-                        </NextLink>
-                      )}
-                      {title === 'Follower' && (
-                        <NextLink
-                          href={{
-                            pathname: `/profile/${fo.users.username}`,
-                            query: { userId: fo.users.id },
-                          }}
-                          passHref
-                        >
-                          {/* <Link color="textPrimary" variant="subtitle2"> */}
-                          {fo.users.username}
-                          {/* </Link> */}
-                        </NextLink>
-                      )}
-                      {/* <Typography
+                    {/* <Typography
                       color="textSecondary"
                       gutterBottom
                       variant="body2"
                     >
                       {connection.commonConnections} connections in common
                     </Typography> */}
-                    </Box>
-                    <IconButton>
-                      <DotsHorizontal fontSize="small" />
-                    </IconButton>
                   </Box>
-                </Paper>
-              </Grid>
-            );
-          })}
+                  <IconButton>
+                    <DotsHorizontal fontSize="small" />
+                  </IconButton>
+                </Box>
+              </Paper>
+            </Grid>
+          ))}
         </Grid>
       </Box>
     </Card>
