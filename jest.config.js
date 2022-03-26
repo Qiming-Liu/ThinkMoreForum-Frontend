b25ddcb91575e5b45878b37236be8995828ddee9
Harris <42738416+devharris7@users.noreply.github.com>
2022-03-26 22:38:58 +1100

    chore: ts config (#145)

--- jest.config.js
+++ jest.config.js
@@ -1,21 +1,19 @@
-module.exports = {
-  verbose: true,
-  moduleFileExtensions: ['js', 'jsx', 'json'],
-  transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
-  },
-  moduleDirectories: ['node_modules', __dirname],
-  clearMocks: true,
-  coverageDirectory: 'coverage',
-  moduleNameMapper: {
-    '^.+\\.svg$': '<rootDir>/utils/svgTransform.js',
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      'test-file-stub',
-    '\\.(css|less|scss)$': 'identity-obj-proxy',
-  },
-  testEnvironment: 'jsdom',
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: './',
+});
+
+// Add any custom config to be passed to Jest
+const customJestConfig = {
+  // Add more setup options before each test is run
+  // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
+  moduleDirectories: ['node_modules', '<rootDir>/'],
+  testEnvironment: 'jest-environment-jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  testMatch: ['**/*.spec.{js,jsx}'],
+  coverageDirectory: 'coverage',
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
   coverageThreshold: {
     global: {
@@ -26,3 +24,6 @@ module.exports = {
     },
   },
 };
+
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+module.exports = createJestConfig(customJestConfig);
