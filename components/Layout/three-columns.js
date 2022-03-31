b8fd5685d7ab3dc3223c64329837b8150eaff92b
zhenhaokk <zhenhao.d.pro@gmail.com>
2022-04-01 01:48:43 +1100

    feat: online user UI

--- components/Layout/three-columns.js
+++ components/Layout/three-columns.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Grid } from '@mui/material';
 import SearchBar from '../SearchBar';
 import RandomPoat from '../RandomPost';
+import OnlineUserStyle from '../OnlineUserStyle';
 
 const ThreeColumns = ({ children }) => {
   return (
     <Grid container direction="row" spacing={2}>
       <Grid item xs={3}>
         <SearchBar />
+        <OnlineUserStyle />
       </Grid>
       <Grid item xs={6}>
         {children}
