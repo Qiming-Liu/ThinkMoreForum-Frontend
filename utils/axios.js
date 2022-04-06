2d955679237f8fa5449c7e8f7e62266f329d3c18
Qiming-Liu <prossliu@gmail.com>
2022-04-06 20:18:23 +0930

    refactor: change back to 403 no logout to see whats happened with jwt

--- utils/axios.js
+++ utils/axios.js
@@ -8,7 +8,7 @@ const getInstance = () => {
   axiosInstance.defaults.baseURL = process.env.NEXT_PUBLIC_BACKEND_URL;
 
   // comment out next line if you want connect online backend
-  axiosInstance.defaults.baseURL = 'https://api.thinkmoreapp.com';
+  // axiosInstance.defaults.baseURL = 'https://api.thinkmoreapp.com';
 
   axiosInstance.defaults.headers.common.Authorization =
     store.getState().sign.token || '';
@@ -24,6 +24,9 @@ const getInstance = () => {
       return config;
     },
     (error) => {
+      // eslint-disable-next-line no-unused-expressions
+      error && console.log(error.response);
+
       // jwt expired
       if (error && error.response && error.response.status === 401) {
         store.dispatch(logoutAction());
@@ -31,13 +34,11 @@ const getInstance = () => {
       }
 
       // jwt invalid
-      if (error && error.response && error.response.status === 403) {
-        store.dispatch(logoutAction());
-        return '';
-      }
+      // if (error && error.response && error.response.status === 403) {
+      //   store.dispatch(logoutAction());
+      //   return '';
+      // }
 
-      // eslint-disable-next-line no-unused-expressions
-      error && console.log(error.response);
       return Promise.reject(error);
     },
   );
