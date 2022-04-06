074ddcb32e78136d92ef23d9742167585fc4c01e
zzyang1994 <yangzhanzhao1994@gmail.com>
2022-04-06 21:57:17 +1000

    feat: index page pinpost static generation

--- components/Categroy/CategoryIntro.js
+++ components/Categroy/CategoryIntro.js
@@ -4,7 +4,7 @@ import { Box, Typography } from '@mui/material';
 const CategoryIntro = (props) => {
   const { categoryTitle, description } = props;
   return (
-    <Box container sx={{ mt: 2 }} vspacing={2}>
+    <Box container sx={{ mt: 2 }}>
       <Typography variant="h4" style={{ fontSize: '34px' }}>
         {categoryTitle}
       </Typography>
