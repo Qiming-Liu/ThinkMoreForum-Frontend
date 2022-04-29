26cca925ee11773a77b20a150a684c8028b40458
Qiming-Liu <prossliu@gmail.com>
2022-04-30 05:09:13 +0930

    fix: edit post ssg

---
+++ pages/api/post-refetch.js
@@ -0,0 +1,11 @@
+export default async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).send('Method Not Allowed');
+  }
+  try {
+    await res.unstable_revalidate(`/post/${req.query.postid}`);
+    return res.json({ revalidated: true });
+  } catch (err) {
+    return res.status(500).send('Error revalidating');
+  }
+}
